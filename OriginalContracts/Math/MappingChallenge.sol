74fb2fa6bdb13f260f1695c2cd0417a04c0aecda
edu san <33841139+econdepe@users.noreply.github.com>
2018-11-23 12:32:32 +0100

    Create MappingChallenge.sol

---
+++ OriginalContracts/Math/MappingChallenge.sol
@@ -0,0 +1,23 @@
+/*
+Who needs mappings? I’ve created a contract that can store key/value pairs using just an array.
+*/
+
+pragma solidity ^0.4.21;
+
+contract MappingChallenge {
+    bool public isComplete;
+    uint256[] map;
+
+    function set(uint256 key, uint256 value) public {
+        // Expand dynamic array as needed
+        if (map.length <= key) {
+            map.length = key + 1;
+        }
+
+        map[key] = value;
+    }
+
+    function get(uint256 key) public view returns (uint256) {
+        return map[key];
+    }
+}
