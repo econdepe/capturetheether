bd1ef23def22b8cf18352c88a44317e409b04b7b
edu san <33841139+econdepe@users.noreply.github.com>
2018-11-23 12:24:24 +0100

    Create GuessTheNumberChallenge.sol

---
+++ OriginalContracts/Lotteries/GuessTheNumberChallenge.sol
@@ -0,0 +1,25 @@
+/*
+I’m thinking of a number. All you have to do is guess it.
+*/
+
+pragma solidity ^0.4.21;
+
+contract GuessTheNumberChallenge {
+    uint8 answer = 42;
+
+    function GuessTheNumberChallenge() public payable {
+        require(msg.value == 1 ether);
+    }
+
+    function isComplete() public view returns (bool) {
+        return address(this).balance == 0;
+    }
+
+    function guess(uint8 n) public payable {
+        require(msg.value == 1 ether);
+
+        if (n == answer) {
+            msg.sender.transfer(2 ether);
+        }
+    }
+}
