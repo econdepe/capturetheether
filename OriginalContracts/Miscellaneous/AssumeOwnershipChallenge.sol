5c649821bb67a1e7c2e68190fb1130df90f5fe28
edu san <33841139+econdepe@users.noreply.github.com>
2018-11-23 12:37:11 +0100

    Create AssumeOwnershipChallenge.sol

---
+++ OriginalContracts/Miscellaneous/AssumeOwnershipChallenge.sol
@@ -0,0 +1,20 @@
+/*
+To complete this challenge, become the owner.
+*/
+
+pragma solidity ^0.4.21;
+
+contract AssumeOwnershipChallenge {
+    address owner;
+    bool public isComplete;
+
+    function AssumeOwmershipChallenge() public {
+        owner = msg.sender;
+    }
+
+    function authenticate() public {
+        require(msg.sender == owner);
+
+        isComplete = true;
+    }
+}
