3f042e509d7c1fee6ac6613badc53951b301e24e
edu san <33841139+econdepe@users.noreply.github.com>
2018-11-23 12:23:23 +0100

    Update CallMeChallenge.sol

--- OriginalContracts/Warmup/CallMeChallenge.sol
+++ OriginalContracts/Warmup/CallMeChallenge.sol
@@ -1,3 +1,9 @@
+/*
+To complete this challenge, all you need to do is call a function.
+
+The “Begin Challenge” button will deploy the following contract:
+*/
+
 pragma solidity ^0.4.21;
 
 contract CallMeChallenge {
