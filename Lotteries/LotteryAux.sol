846f7407c2a8afca5842c9668f880c4f5f31f7ac
edu san <33841139+econdepe@users.noreply.github.com>
2018-12-11 13:30:35 +0100

    Update LotteryAux.sol

--- Lotteries/LotteryAux.sol
+++ Lotteries/LotteryAux.sol
@@ -2,13 +2,16 @@ pragma solidity ^0.4.21;
 
 contract LotteryAux {
     address owner;
-
+    
+    // define owner
     constructor() public {
         owner = msg.sender;
     }
     
+    // fallback function. Makes the contract able to receive ether
     function () public payable {}
     
+    // allow withdrawal of contract funds (only to the owner)
     function withdraw() public {
         require(msg.sender == owner);
         owner.transfer(address(this).balance);
